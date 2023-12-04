c5697a10bf174522da58e7201c7fd05dc3cc41a1
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-12-04 20:21:42 +0000

    add lollipop image to the hero section

--- src/app/HeroSection.tsx
+++ src/app/HeroSection.tsx
@@ -1,21 +1,24 @@
+import Image from "next/image";
+
 export const HeroSection = () => {
   return (
-    <div className="flex-grow p-32">
-      <div className="flex flex-col gap-6">
-        <h1 className="text-7xl font-black uppercase italic text-pink-500">
-          Namukilke
-        </h1>
-        <div className="flex w-2/3 flex-col gap-2 text-pink-400">
-          <p className="font-semibold italic">
-            Namukilke users have bought a total of 65 products this week,
-            costing 93,53€
-          </p>
-          <p>
-            For more statistics, see the{" "}
-            <span className="italic underline">stats</span> page
+      <section className="flex flex-grow px-32 h-full">
+        <div className="flex w-2/3 flex-col gap-2 items justify-center">
+          <h1 className="text-7xl font-black uppercase italic text-pink-500">
+            Namukilke
+          </h1>
+          <p className="font-semibold italic text-pink-400">
+            Welcome to Aalto's greatest snack store!
           </p>
         </div>
-      </div>
-    </div>
+        <div className="w-1/3 flex-grow relative">
+          <Image
+            src="/lollipop.png"
+            alt="Lollipop"
+            className="object-contain"
+            fill
+            />
+          </div>
+      </section>
   );
 };
