e6c4185247ce085466c90b996d7860c6d8a5d40b
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-12-06 20:00:36 +0000

    adjust styles for infocard

--- src/components/ui/InfoCard.tsx
+++ src/components/ui/InfoCard.tsx
@@ -1,8 +1,6 @@
 import { type ComponentProps } from "react";
 import { type IconType } from "react-icons";
 
-import * as AspectRatio from "@radix-ui/react-aspect-ratio";
-
 interface Props extends ComponentProps<"div"> {
   title: string;
   data: string;
@@ -11,15 +9,19 @@ interface Props extends ComponentProps<"div"> {
 
 export const InfoCard = ({ title, data, Icon, ...props }: Props) => {
   return (
-    <AspectRatio.Root
-      ratio={16 / 9}
-      className="flex min-w-full flex-col gap-2 rounded-xl border-[3px] border-primary-400 bg-primary-50 p-8"
+    <div
+      {...props}
+      className="flex min-w-full flex-col gap-3 rounded-xl border-[3px] border-primary-400 bg-primary-50 p-8"
     >
       <div className="flex h-min w-full justify-between gap-2">
-        <span className="text-2xl capitalize text-primary-400">{title}</span>
+        <span className="text text-2xl capitalize text-primary-400">
+          {title}
+        </span>
         <Icon className="text-primary-500" size={36} />
       </div>
-      <span className="text-5xl font-bold text-primary-500">{data}</span>
-    </AspectRatio.Root>
+      <span className="text-5xl font-bold tracking-tight text-primary-500">
+        {data}
+      </span>
+    </div>
   );
 };
