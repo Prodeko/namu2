01053282d10ef76632002b704c74469b7b7b51c9
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-12-06 14:16:17 +0000

    add boilerplate stats page

---
+++ src/app/(loggedin)/stats/page.tsx
@@ -0,0 +1,11 @@
+import { SectionTitle } from "@/components/ui/SectionTitle";
+
+const StatsPage = () => {
+  return (
+    <div className="h-full w-full flex-grow bg-white py-12">
+      <SectionTitle className="px-12" title="Stats dashboard" />
+    </div>
+  );
+};
+
+export default StatsPage;
