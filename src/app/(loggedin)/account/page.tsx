609198a35b92b6f1cbcd439fbd89b43a9783a325
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-12-06 19:58:33 +0000

    add linebuttons to account page

--- src/app/(loggedin)/account/page.tsx
+++ src/app/(loggedin)/account/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { HiLogout } from "react-icons/hi";
 import { HiWallet, HiXCircle } from "react-icons/hi2";
 
 import { FatButton } from "@/components/ui/Buttons/FatButton";
+import { LineButton } from "@/components/ui/Buttons/LineButton";
 import { InfoCard } from "@/components/ui/InfoCard";
 import { SectionTitle } from "@/components/ui/SectionTitle";
 
 const AccountPage = () => {
+  const pathName = usePathname();
   return (
     <div className="flex h-full w-full flex-grow flex-col justify-between bg-white py-12">
       <div className="flex flex-col gap-9">
@@ -15,6 +18,25 @@ const AccountPage = () => {
         <div className="grid grid-cols-2 gap-12 px-12">
           <InfoCard title="wallet" data="0,99 €" Icon={HiWallet} />
           <InfoCard title="RFID" data="Disconnected" Icon={HiXCircle} />
+        </div>
+        <div className="flex flex-col">
+          <LineButton
+            text="Change PIN code"
+            buttonType="a"
+            href={`${pathName}/change-pincode`}
+          />
+          <LineButton text="Add funds" buttonType="button" />
+          <LineButton text="Connect RFID" buttonType="button" />
+          <LineButton
+            text="Purchase history"
+            buttonType="a"
+            href={`${pathName}/purchase-history`}
+          />
+          <LineButton
+            text="Transaction history"
+            buttonType="a"
+            href={`${pathName}/transaction-history`}
+          />
         </div>
       </div>
 
