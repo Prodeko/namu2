f3fc29d65a792cdc17819c9ccfacccdf602e9065
root <uusipaikka.rasmus@gmail.com>
2023-12-10 18:58:21 +0000

    Fixed format date function

--- src/components/ui/WishItem.tsx
+++ src/components/ui/WishItem.tsx
@@ -13,8 +13,8 @@ interface Props extends WishItemProps {
 }
 
 const formatDate = (date: Date) => {
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).toString();
+  const month = String(date.getMonth() + 1).toString();
   const year = date.getFullYear();
 
   return `${day}/${month}/${year}`;
