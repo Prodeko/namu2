edf6b3ae662cd1db9a431b97e966966b45ebd18d
root <uusipaikka.rasmus@gmail.com>
2023-12-10 15:37:20 +0000

    Added a menu button for wish page menu

---
+++ src/components/ui/WishNavButton.tsx
@@ -0,0 +1,29 @@
+import { type VariantProps, cva } from "class-variance-authority";
+import { type ComponentProps } from "react";
+
+const buttonStyles = cva("w-full px-6 py-3 text-center text-3xl", {
+  variants: {
+    intent: {
+      active: "border-b-2 border-black font-bold text-black",
+      regular: "border-b-0 text-gray-700",
+    },
+  },
+  defaultVariants: {
+    intent: "regular",
+  },
+});
+
+type WishNavProps = ComponentProps<"button">;
+
+export interface Props extends WishNavProps {
+  name: string;
+  intent: string;
+}
+
+export const WishNavButton = ({ name, intent, ...props }: Props) => {
+  return (
+    <button className={buttonStyles({ intent })} {...props}>
+      {name}
+    </button>
+  );
+};
