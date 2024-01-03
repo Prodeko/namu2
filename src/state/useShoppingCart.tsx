2003bc4a680e60b3b3dc7297ac2cc899c250a69f
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-01-03 13:29:50 +0000

    Back to synced local storage

--- src/state/useShoppingCart.tsx
+++ src/state/useShoppingCart.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import type { CartProduct } from "@/common/types";
-
-import { useLocalStorage } from "./useLocalStorage";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
 export const useShoppingCart = () => {
   const [cart, setCart] = useLocalStorage<CartProduct[]>("shoppingCart", []);
