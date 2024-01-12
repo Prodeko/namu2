ad7b75afd3ee079fef22d657ed18b397042cdeb9
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-01-12 09:46:45 +0000

    Adjust wish text based on the page

--- src/app/(loggedin)/wish/layout.tsx
+++ src/app/(loggedin)/wish/layout.tsx
@@ -1,8 +1,12 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 import { SectionTitle } from "@/components/ui/SectionTitle";
 
 const WishLayout = ({ children }: { children: ReactNode }) => {
+  const pathName = usePathname();
   return (
     <div className="flex min-h-0 flex-1 flex-col items-center">
       <div className="relative flex flex-none flex-col items-center justify-center gap-3 overflow-hidden bg-[radial-gradient(circle_at_50.00%_50.00%,rgba(249,227,239,1)_0%,rgba(236,175,208,1)_100%)] px-24 py-24 lg:px-40">
@@ -32,9 +36,9 @@ const WishLayout = ({ children }: { children: ReactNode }) => {
           className="z-10 text-center text-4xl font-bold text-primary-600"
         />
         <p className="z-10 inline-flex text-center text-2xl">
-          Don&apos;t worry, Namu CEO is here for you! Just drop your wishes in
-          the following form and you might find the product in our shelves in
-          the upcoming weeks. 😎
+          {pathName === "/wish/new"
+            ? "Don't worry, Namu CEO is here for you! Just drop your wishes in the following form and you might find the product in our shelves in the upcoming weeks. 😎"
+            : "Here you can inspect new product candidates and vote for them to be added to the catalogue. You can also create your very own wish by clicking the button at the bottom!"}
         </p>
       </div>
       {children}
