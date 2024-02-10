bf9bff7b51959b92b97f5ed6b9dcf21e1cb24381
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-02-10 13:19:00 +0000

    Separate loginform as its own client component

---
+++ src/app/(loggedout)/LoginForm.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useFormState } from "react-dom";
+import { HiLogin } from "react-icons/hi";
+
+import { FatButton } from "@/components/ui/Buttons/FatButton";
+import { Input } from "@/components/ui/Input";
+import { loginAction } from "@/server/actions/auth";
+
+export const LoginForm = () => {
+  const [state, formAction] = useFormState(loginAction, {
+    userName: "",
+    pinCode: "",
+    message: "",
+  });
+
+  return (
+    <form action={formAction} className="flex w-full flex-col gap-6">
+      <Input
+        labelText="Namu ID"
+        placeholderText={"Namu Käyttäjä"}
+        name="userName"
+        required
+      />
+      <Input
+        labelText="Pin Code"
+        type="number"
+        placeholderText={"1234"}
+        name="pinCode"
+        required
+      />
+      <FatButton
+        buttonType="button"
+        type="submit"
+        text="Login"
+        intent="primary"
+        RightIcon={HiLogin}
+      />
+    </form>
+  );
+};
--- src/app/(loggedout)/page.tsx
+++ src/app/(loggedout)/page.tsx
@@ -1,24 +1,13 @@
-"use client";
-
-import { useFormState } from "react-dom";
-import { HiLogin, HiOutlineUserAdd } from "react-icons/hi";
+import { HiOutlineUserAdd } from "react-icons/hi";
 
 import { BottomCard } from "@/components/ui/BottomCard";
-import { FatButton } from "@/components/ui/Buttons/FatButton";
 import { ThinButton } from "@/components/ui/Buttons/ThinButton";
-import { Input } from "@/components/ui/Input";
 import { PromptText } from "@/components/ui/PromptText";
-import { loginAction } from "@/server/actions/auth";
 
 import { HeroSection } from "./HeroSection";
+import { LoginForm } from "./LoginForm";
 
 const Home = () => {
-  const [state, formAction] = useFormState(loginAction, {
-    userName: "",
-    pinCode: "",
-    message: "",
-  });
-
   return (
     <>
       <HeroSection />
@@ -26,22 +15,7 @@ const Home = () => {
         <h2 className="text-4xl font-bold text-neutral-700">
           Login to Your Account
         </h2>
-        <form action={formAction} className="flex w-full flex-col gap-6">
-          <Input placeholderText={"Namu ID"} name="userName" required />
-          <Input
-            type="number"
-            placeholderText={"PIN"}
-            name="pinCode"
-            required
-          />
-          <FatButton
-            buttonType="button"
-            type="submit"
-            text="Login"
-            intent="primary"
-            RightIcon={HiLogin}
-          />
-        </form>
+        <LoginForm />
         <div className="flex w-full items-center justify-end gap-4">
           <PromptText sizing="2xl" text="Don't have an account?" />
           <ThinButton
