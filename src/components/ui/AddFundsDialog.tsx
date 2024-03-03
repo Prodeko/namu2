bb09e369ee41c14cd5601d3288aec359e6a9c82d
ccruzkauppila <ccruzkauppila@live.com>
2024-03-03 15:23:37 +0000

    Add steps to addFunds dialog

--- src/components/ui/AddFundsDialog.tsx
+++ src/components/ui/AddFundsDialog.tsx
@@ -23,7 +23,8 @@ interface Props {
 
 export const AddFundsDialog = ({ children }: Props) => {
   const [amountToAdd, setAmountToAdd] = useState(0);
-
+  const [step, setStep] = useState(0);
+  const steps = [AddFundsStep1, AddFundsStep2];
   const {
     containerAnimation,
     overlayAnimation,
@@ -31,7 +32,20 @@ export const AddFundsDialog = ({ children }: Props) => {
     setOpen,
     toggleContainer,
   } = useSlideinAnimation();
-
+  const currentStep = () => {
+    const Current = steps[step];
+    return (
+      <Current amountToAdd={amountToAdd} setAmountToAdd={setAmountToAdd} />
+    );
+  };
+  const augmentStep = () => {
+    if (step < steps.length - 1) setStep(step + 1);
+    else setOpen(false);
+  };
+  const decreaseStep = () => {
+    if (step > 0) setStep(step - 1);
+    else setOpen(false);
+  };
   return (
     <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Trigger asChild>{children}</Dialog.Trigger>
@@ -42,21 +56,28 @@ export const AddFundsDialog = ({ children }: Props) => {
         />
         <AnimatedDialog
           style={containerAnimation}
-          className="fixed top-0 z-20 flex h-full w-full items-center justify-center"
+          className="fixed top-0 z-20 flex h-full w-full items-center justify-center px-6 md:px-12"
         >
-          <div className="flex flex-col items-center gap-12 rounded-[40px] bg-neutral-50 px-16 py-12">
+          <div className="flex w-full  flex-col items-center gap-12 rounded-2xl bg-neutral-50 px-12 py-12">
             <h2 className="mt-6 text-5xl font-bold text-neutral-700">
               Add Funds
             </h2>
             <hr className="h-px w-full bg-neutral-300 opacity-90" />
-            <AddFundsStep1
-              amountToAdd={amountToAdd}
-              setAmountToAdd={(value) => setAmountToAdd(value)}
-            />
-            <AddFundsStep2
-              amountToAdd={amountToAdd}
-              setAmountToAdd={setAmountToAdd}
-            />
+            {currentStep()}
+            <div className="mt-6 flex w-full gap-6">
+              <FatButton
+                buttonType="button"
+                text="Cancel"
+                intent="secondary"
+                onClick={decreaseStep}
+              />
+              <FatButton
+                buttonType="button"
+                text="Proceed"
+                intent="primary"
+                onClick={augmentStep}
+              />
+            </div>
           </div>
         </AnimatedDialog>
       </Dialog.Portal>
@@ -87,6 +108,7 @@ const AddFundsStep1 = ({ amountToAdd, setAmountToAdd }: StepProps) => {
       />
       <Input
         placeholderText="Custom amount..."
+        className="w-full"
         value={amountToAdd}
         onChange={(e) => {
           handleValueChange(e.target.value);
@@ -94,29 +116,17 @@ const AddFundsStep1 = ({ amountToAdd, setAmountToAdd }: StepProps) => {
         }}
         type="number"
       />
-      <div className="mt-6 flex w-full gap-6">
-        <Dialog.Close asChild>
-          <FatButton buttonType="button" text="Cancel" intent="secondary" />
-        </Dialog.Close>
-        <FatButton buttonType="button" text="Proceed" intent="primary" />
-      </div>
     </>
   );
 };
 
 const AddFundsStep2 = ({ amountToAdd, setAmountToAdd }: StepProps) => {
   const getMobilePayLink = (sum) =>
-    `https://mobilepay.fi/Yrityksille/Maksulinkki/maksulinkki-vastaus?phone=43477&amount=${sum}&comment=Namu&lock=1`;
+    `https://mobilepay.fi/Yrityksille/Maksulinkki/maksulinkki-vastaus?phone=43477&amount=${sum}&comment=Namutalletus&lock=1`;
   return (
     <>
       <QRCodeSVG value={getMobilePayLink(amountToAdd)} size={200} />
       <p className="text-2xl">Scan the following code with MobilePay to pay</p>
-      <div className="mt-6 flex w-full gap-6">
-        <Dialog.Close asChild>
-          <FatButton buttonType="button" text="Cancel" intent="secondary" />
-        </Dialog.Close>
-        <FatButton buttonType="button" text="Done" intent="primary" />
-      </div>
     </>
   );
 };
