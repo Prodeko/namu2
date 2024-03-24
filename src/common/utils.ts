cfd217b20ef1ce548698246911722bc351981817
Rasmus <uusipaikka.rasmus@gmail.com>
2024-03-24 15:18:23 +0000

    added helper functions for dates and times

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -13,6 +13,19 @@ export const formatCurrency = (price: number) => {
   }).format(price);
 };
 
+export const formatDate = (date) => {
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // January is 0!
+  const year = date.getFullYear();
+  return `${day}.${month}.${year}`;
+};
+
+export const formatTime = (date) => {
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  return `${hours}:${minutes}`;
+};
+
 /**
  * Throws an error if the code is running on the server.
  * @param errorMessage - The error message to throw.
