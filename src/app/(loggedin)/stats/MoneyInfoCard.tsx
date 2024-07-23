d01ef3f1a42269f1169bd8f878f73878c17b83d2
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-07-23 08:00:47 +0000

    Create info card component for money spent

---
+++ src/app/(loggedin)/stats/MoneyInfoCard.tsx
@@ -0,0 +1,40 @@
+import { HiCurrencyDollar } from "react-icons/hi";
+
+import type { Timeframe } from "@/common/types";
+import { InfoCard, InfoCardLoading } from "@/components/ui/InfoCard";
+import { useQuery } from "@tanstack/react-query";
+
+export const MoneyInfoCard = ({ timeFrame }: { timeFrame: Timeframe }) => {
+  const title = "Money spent";
+  const { data, isLoading, isError } = useQuery<number>({
+    queryKey: ["stats-transaction-money-spent", timeFrame],
+    queryFn: async () => {
+      const query = await fetch(
+        "/api/stats/transactions/money?" +
+          new URLSearchParams({ timeFrame }).toString(),
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      const json = await query.json();
+      return json.data;
+    },
+  });
+
+  if (isLoading) {
+    return <InfoCardLoading title={title} Icon={HiCurrencyDollar} />;
+  }
+
+  if (isError) {
+    return (
+      <InfoCard title={title} data={"Not found"} Icon={HiCurrencyDollar} />
+    );
+  }
+
+  return (
+    <InfoCard title={title} data={data.toString()} Icon={HiCurrencyDollar} />
+  );
+};
