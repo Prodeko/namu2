26ec1bb35bd713b55d1d0a5bd29ca665ed22a675
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-07-26 13:22:53 +0000

    Fix bug where only one of the product categories is returned

--- src/app/api/product/categories/route.ts
+++ src/app/api/product/categories/route.ts
@@ -9,19 +9,18 @@ export async function GET() {
 
   const result = (await db.$queryRaw`
     WITH products AS (
-      SELECT "category" as categoryName, "name" as nodeName, "id" as nodeId
+      SELECT "category", "name" as nodeName, "id" as nodeId
       FROM "Product"
       ORDER BY "category", "name"
     )
 
-    SELECT categoryName, JSON_AGG(JSON_BUILD_OBJECT('nodeName', nodeName, 'nodeId', nodeId)) as nodes
+    SELECT category, JSON_AGG(JSON_BUILD_OBJECT('nodeName', nodeName, 'nodeId', nodeId)) as nodes
     FROM products
     GROUP BY 1
-    LIMIT 1;
   `) as any[];
 
   const parsedResult = result.map((row) => ({
-    categoryName: row.categoryname,
+    categoryName: row.category,
     nodes: row.nodes,
   }));
 
