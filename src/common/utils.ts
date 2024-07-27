240a571eff39fcd77157bc6dd7c882889d47a17e
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-07-27 15:07:38 +0000

    Add helper function for parsing isostrings

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -49,3 +49,8 @@ export const errorOnServerEnvironment = (errorMessage: string) => {
     throw new Error(errorMessage);
   }
 };
+
+export const parseISOString = (s: string) => {
+  const b = s.split(/\D+/);
+  return new Date(Date.UTC(b[0], b[1], b[2], b[3], b[4], b[5], b[6]));
+};
