a957f99695377323e44787b1186891b41a419839
Risto Virtaharju <rvirtaha@hotmail.com>
2024-07-31 20:08:28 +0000

    add base skeleton for AzureBlobService

---
+++ src/common/azureBlobService.ts
@@ -0,0 +1,35 @@
+import {
+  BlobClient,
+  BlobDownloadResponseParsed,
+  BlobServiceClient,
+  ContainerClient,
+} from "@azure/storage-blob";
+
+class AzureBlobService {
+  private blobServiceClient: BlobServiceClient;
+  private containerClient: ContainerClient;
+
+  /**
+   * Create a AzureBlobService either for server-side or client-side use.
+   * If you pass in client-side credential, then only read methods will be available,
+   * assuming your client SAS token's permissions are configured correctly.
+   */
+  constructor(
+    SERVER_AZURE_BLOB_SAS_URL: string,
+    AZURE_BLOB_CONTAINER_NAME: string,
+  ) {
+    this.blobServiceClient = new BlobServiceClient(SERVER_AZURE_BLOB_SAS_URL);
+    this.containerClient = this.blobServiceClient.getContainerClient(
+      AZURE_BLOB_CONTAINER_NAME,
+    );
+  }
+
+  public async getBlob(blobName: string): Promise<Blob | undefined> {
+    const blobClient: BlobClient = this.containerClient.getBlobClient(blobName);
+    const blobResult: BlobDownloadResponseParsed = await blobClient.download();
+    const body = blobResult.blobBody;
+    return body;
+  }
+}
+
+export default AzureBlobService;
