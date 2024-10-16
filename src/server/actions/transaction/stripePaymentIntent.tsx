cb8ed5634c5d2157a5265fef9a0544ac122431e4
ccruzkauppila <ccruzkauppila@live.com>
2024-10-16 11:26:37 +0000

    Added Stripe paymentIntent

---
+++ src/server/actions/transaction/stripePaymentIntent.tsx
@@ -0,0 +1,16 @@
+"use server";
+
+import Stripe from "stripe";
+
+import { serverEnv } from "@/env/server.mjs";
+
+const stripe = new Stripe(serverEnv.STRIPE_TESTMODE_SECRET_KEY);
+
+export const stripePaymentIntentAction = async (amount: number) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "eur",
+    payment_method_types: ["card"],
+  });
+  return paymentIntent.client_secret as string;
+};
