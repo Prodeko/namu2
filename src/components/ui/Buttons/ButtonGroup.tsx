810870960e61d0e9c7f510e8e846dafb72b3ea5a
ccruzkauppila <ccruzkauppila@live.com>
2024-10-17 12:00:59 +0000

    Add label to buttonGroup

--- src/components/ui/Buttons/ButtonGroup.tsx
+++ src/components/ui/Buttons/ButtonGroup.tsx
@@ -2,12 +2,15 @@ import { cva } from "class-variance-authority";
 import { useEffect, useState } from "react";
 import { HiMinus, HiPlus } from "react-icons/hi";
 
-interface Props {
+import { ComponentPropsWithRef } from "@react-spring/web";
+
+interface Props extends ComponentPropsWithRef<"input"> {
   leftButtonAction: () => void;
   rightButtonAction: () => void;
   onInputChange: (value: number) => void;
   inputValue: number;
-  maxValue: number;
+  maxValue?: number;
+  labelText?: string;
 }
 
 const buttonStyles = cva(
@@ -19,7 +22,9 @@ export const ButtonGroup = ({
   rightButtonAction,
   onInputChange,
   inputValue,
-  maxValue,
+  maxValue = 99999,
+  labelText,
+  ...props
 }: Props) => {
   const [textValue, setTextValue] = useState<string>(inputValue.toString());
   const iconStyles = cva("h-7 w-7");
@@ -44,31 +49,40 @@ export const ButtonGroup = ({
     setTextValue(inputValue.toString());
   }, [inputValue]);
 
+  const styles = cva(`flex flex-col gap-1 ${props.className || ""}`);
+
   return (
-    <div className="flex items-center justify-between gap-3">
-      <button
-        className={buttonStyles()}
-        disabled={inputValue === 1}
-        type="button"
-        onClick={leftButtonAction}
-      >
-        <HiMinus className={iconStyles()} />
-      </button>
-      <input
-        type="number"
-        value={textValue}
-        onBlur={handleBlur}
-        onChange={handleInputChange}
-        className="hide-spinner flex h-12 w-12 appearance-none items-center justify-center rounded bg-primary-100 text-center text-2xl font-medium text-primary-900 outline-primary-700"
-      />
-      <button
-        type="button"
-        onClick={rightButtonAction}
-        className={buttonStyles()}
-        disabled={inputValue >= maxValue}
-      >
-        <HiPlus className={iconStyles()} />
-      </button>
+    <div className={styles()}>
+      {labelText && (
+        <span className="w-fit cursor-pointer text-base font-normal text-neutral-500 transition-all group-focus-within:font-medium group-focus-within:text-primary-500">
+          {labelText}
+        </span>
+      )}
+      <div className="flex items-center justify-between gap-3">
+        <button
+          className={buttonStyles()}
+          disabled={inputValue === 1}
+          type="button"
+          onClick={leftButtonAction}
+        >
+          <HiMinus className={iconStyles()} />
+        </button>
+        <input
+          type="number"
+          value={textValue}
+          onBlur={handleBlur}
+          onChange={handleInputChange}
+          className="hide-spinner flex h-12 w-12 appearance-none items-center justify-center rounded bg-primary-100 text-center text-2xl font-medium text-primary-900 outline-primary-700"
+        />
+        <button
+          type="button"
+          onClick={rightButtonAction}
+          className={buttonStyles()}
+          disabled={inputValue >= maxValue}
+        >
+          <HiPlus className={iconStyles()} />
+        </button>
+      </div>
     </div>
   );
 };
