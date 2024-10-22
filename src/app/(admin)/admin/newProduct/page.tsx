1ffc5602b2584a57861b203a27870494d847491a
ccruzkauppila <ccruzkauppila@live.com>
2024-10-22 10:12:46 +0000

    Responsive UI for admin new product page

--- src/app/(admin)/admin/newProduct/page.tsx
+++ src/app/(admin)/admin/newProduct/page.tsx
@@ -5,7 +5,7 @@ import { EditProductForm } from "@/components/ui/EditProductForm";
 
 const NewProduct = () => {
   return (
-    <div className="flex w-[80%] max-w-screen-lg flex-col gap-8">
+    <div className="flex w-[80%] max-w-screen-lg flex-col gap-8 pb-6">
       <EditProductForm />
     </div>
   );
