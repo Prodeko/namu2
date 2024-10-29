2d8fd196fac45493adfd42477872707cf6a53302
ccruzkauppila <ccruzkauppila@live.com>
2024-10-29 13:11:10 +0000

    No autocapitalization or spellcheck on username

--- src/app/(loggedout)/login/LoginForm.tsx
+++ src/app/(loggedout)/login/LoginForm.tsx
@@ -53,6 +53,8 @@ export const LoginForm = () => {
           labelText="Namu ID"
           placeholder={"Namu Käyttäjä"}
           name="userName"
+          autoCapitalize="none"
+          spellCheck="false"
           required
         />
         <InputWithLabel
