4e0efde24e7151c34aa58f091df23197d646df1b
ccruzkauppila <ccruzkauppila@live.com>
2024-11-05 13:16:55 +0000

    Simplify adding products to cart

--- src/components/ui/ProductSection.tsx
+++ src/components/ui/ProductSection.tsx
@@ -4,8 +4,10 @@ import { type ComponentPropsWithoutRef } from "react";
 
 import { type Section } from "@/common/types";
 import { type ClientProduct } from "@/common/types";
+import { useShoppingCart } from "@/state/useShoppingCart";
 import { useSyncActiveSection } from "@/state/useSyncActiveSection";
 
+import { ListItem } from "./ListItem";
 import { ProductModal } from "./ProductModal";
 import { SectionTitle } from "./SectionTitle";
 
@@ -18,6 +20,21 @@ export interface Props extends SectionProps {
 
 export const ProductSection = ({ section, items, ...props }: Props) => {
   const ref = useSyncActiveSection(section);
+  const { updateCart, getItemById } = useShoppingCart();
+
+  const addToCart = (product: ClientProduct) => {
+    const itemCount = getItemById(product.id)?.quantity || 0;
+    updateCart({
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      quantity: itemCount + 1,
+      imageFilePath: product.imageFilePath,
+      category: product.category,
+      description: product.description,
+      stock: product.stock,
+    });
+  };
 
   return (
     <section
@@ -29,7 +46,11 @@ export const ProductSection = ({ section, items, ...props }: Props) => {
       <SectionTitle className="px-5 md:px-12" title={section.name} />
       <ul className="flex flex-col divide-y-2 divide-neutral-200">
         {items.map((item) => (
-          <ProductModal key={item.id} product={item} />
+          <ListItem
+            key={item.id}
+            product={item}
+            onClick={() => addToCart(item)}
+          />
         ))}
       </ul>
     </section>
