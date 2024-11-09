60016a3f831c90b45e044e2f6c30df38b52a8a8d
ccruzkauppila <ccruzkauppila@live.com>
2024-11-09 17:44:43 +0000

    add navigator component for admin stats page

--- src/app/(admin)/admin/statistics/page.tsx
+++ src/app/(admin)/admin/statistics/page.tsx
@@ -1,26 +1,78 @@
-import { cva } from "class-variance-authority";
+import {
+  endOfDay,
+  endOfMonth,
+  endOfWeek,
+  endOfYear,
+  setDefaultOptions,
+} from "date-fns";
 import { Suspense } from "react";
 
-import { AdminTitle } from "@/components/ui/AdminTitle";
 import { cn } from "@/lib/utils";
 
 import { AdminProductStatistics } from "./AdminProductStatistics";
+import { AdminStatsNavigator } from "./AdminStatsNavigator";
 import { DepositNumbersCard } from "./DepositNumbersCard";
 import { KeyNumbers } from "./KeyNumbers";
 import { SalesNumbersCard } from "./SalesNumbersCard";
-import { StatisticsList } from "./StatisticsList";
+
+setDefaultOptions({
+  weekStartsOn: 1,
+});
 
 const containerStyles =
   "w-full rounded-md divide-y-2 divide-neutral-600 border-2 border-neutral-600 bg-white text-neutral-800";
 
-const Statistics = () => {
+export type StatsPeriod = "daily" | "weekly" | "monthly" | "yearly";
+export type StatsTimeframe = {
+  startDate: Date;
+  endDate: Date;
+};
+
+const parseDateFromString = (milliseconds: string): Date => {
+  const ms = parseInt(milliseconds);
+  if (!Number.isNaN(ms)) return new Date(ms);
+  return new Date();
+};
+
+const getEndDate = (startDate: Date, period: StatsPeriod): Date => {
+  switch (period) {
+    case "daily":
+      return endOfDay(startDate);
+    case "weekly":
+      return endOfWeek(startDate);
+    case "monthly":
+      return endOfMonth(startDate);
+    case "yearly":
+      return endOfYear(startDate);
+  }
+};
+
+const Statistics = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) => {
+  const activePeriod = (await searchParams).period as StatsPeriod;
+  const startingFrom = (await searchParams).startingFrom as string;
+  const startDate = startingFrom
+    ? parseDateFromString(startingFrom)
+    : new Date(Date.now() - 24 * 60 * 60 * 1000);
+  const endDate = getEndDate(startDate, activePeriod);
+  const timeframe = { startDate, endDate } as StatsTimeframe;
+
   return (
     <div className="no-scrollbar grid h-fit w-full grid-cols-3 gap-10 overflow-y-scroll px-0 pb-12 lg:w-[80%]">
-      <AdminTitle title="Statistics" className="col-span-full " />
+      <AdminStatsNavigator activePeriod={activePeriod} startDate={startDate} />
       <div className="col-span-2 flex flex-col gap-10 self-start">
         <Suspense fallback={<p> Loading stats...</p>}>
-          <SalesNumbersCard className={cn("w-full", containerStyles)} />
-          <DepositNumbersCard className={cn("w-full", containerStyles)} />
+          <SalesNumbersCard
+            timeframe={timeframe}
+            className={cn("w-full", containerStyles)}
+          />
+          <DepositNumbersCard
+            timeframe={timeframe}
+            className={cn("w-full", containerStyles)}
+          />
         </Suspense>
       </div>
       <Suspense fallback={<p> Loading key figures...</p>}>
