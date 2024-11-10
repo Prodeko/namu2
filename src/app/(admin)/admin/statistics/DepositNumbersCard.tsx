baa6dc2b31c74405c78cf3ff9d74cecf2117cc09
ccruzkauppila <ccruzkauppila@live.com>
2024-11-10 10:13:29 +0000

    Show timeseries data for deposits

--- src/app/(admin)/admin/statistics/DepositNumbersCard.tsx
+++ src/app/(admin)/admin/statistics/DepositNumbersCard.tsx
@@ -1,31 +1,68 @@
+import { format, getWeek } from "date-fns";
 import { ComponentPropsWithoutRef } from "react";
 
 import { formatCurrency } from "@/common/utils";
 import { cn } from "@/lib/utils";
-import { getDepostitData } from "@/server/actions/stats/deposits";
-import { getTransactionStats } from "@/server/actions/stats/transactions";
+import {
+  getDepositStatsByDay,
+  getDepositStatsByHour,
+  getDepositStatsByMonth,
+  getDepositStatsByWeek,
+  getDepostitData,
+} from "@/server/actions/stats/deposits";
+import {
+  TimeseriesDatapoint,
+  getTransactionStats,
+} from "@/server/actions/stats/transactions";
 
 import { HeadlinerStatistic } from "./HeadlinerStatistic";
+import { ChartDataGetter } from "./SalesNumbersCard";
 import { AdminBarChart } from "./charts/AdminBarChart";
-import { StatsTimeframe } from "./page";
+import { StatsPeriod, StatsTimeframe } from "./page";
 
 interface Props extends ComponentPropsWithoutRef<"div"> {
   timeframe: StatsTimeframe;
 }
+
+const getData: Record<StatsPeriod, ChartDataGetter> = {
+  daily: {
+    dataGetter: getDepositStatsByHour,
+    labelGetter: (d: TimeseriesDatapoint) => format(d.date, "HH:mm"),
+  },
+  weekly: {
+    dataGetter: getDepositStatsByDay,
+    labelGetter: (d: TimeseriesDatapoint) => format(d.date, "EEEE"),
+  },
+  monthly: {
+    dataGetter: getDepositStatsByWeek,
+    labelGetter: (d: TimeseriesDatapoint) => `Week ${getWeek(d.date)}`,
+  },
+  yearly: {
+    dataGetter: getDepositStatsByMonth,
+    labelGetter: (d: TimeseriesDatapoint) => format(d.date, "MMM"),
+  },
+};
 
 export const DepositNumbersCard = async ({ timeframe, ...props }: Props) => {
   const depositStats = await getDepostitData(
     timeframe.startDate,
     timeframe.endDate,
   );
+  const chartDataGetter = getData[timeframe.activePeriod];
+  const chartData = await chartDataGetter.dataGetter(
+    timeframe.startDate,
+    timeframe.endDate,
+  );
+  const chartLabels = chartData.map(chartDataGetter.labelGetter);
+  const datapoints = chartData.map((p) => p.value);
   return (
     <div className={cn(" grid grid-cols-3", props.className)}>
       <h2 className="col-span-3 px-4 py-5 text-3xl font-bold">Deposits</h2>
 
       <div className="col-span-2 flex flex-col p-4">
         <AdminBarChart
-          data={[65, 123, 54, 15, 27]}
-          labels={["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]}
+          data={datapoints}
+          labels={chartLabels}
           className="h-64 w-full self-center pl-6 pt-6"
         />
       </div>
--- src/server/actions/stats/deposits.ts
+++ src/server/actions/stats/deposits.ts
@@ -2,6 +2,8 @@
 
 import { db } from "@/server/db/prisma";
 
+import { TimeseriesDatapoint } from "./transactions";
+
 export type DepositStats = {
   amount: number;
   sum: number;
@@ -41,4 +43,132 @@ export const getDepostitData = async (
     sum: result._sum.amount?.toNumber() || 0,
     average: result._avg.amount?.toNumber() || 0,
   };
+};
+
+/**
+ * Gets the total sum of deposits netween the given dates grouped by month
+ * @param startDate
+ * @param endDate
+ * @returns {Promise<TimeseriesDatapoint[]>} The total sum of deposits between the given dates grouped by month
+ */
+export const getDepositStatsByMonth = async (
+  startDate: Date,
+  endDate: Date,
+): Promise<TimeseriesDatapoint[]> => {
+  const result = await db.$queryRaw`
+  SELECT
+    date_series.month AS date,
+    COALESCE(SUM("amount"), 0) AS value
+  FROM
+    generate_series(
+      DATE_TRUNC('month', ${startDate}::date),
+      DATE_TRUNC('month', ${endDate}::date),
+      INTERVAL '1 month'
+    ) AS date_series(month)
+  LEFT JOIN
+    "Deposit" ON DATE_TRUNC('month', "createdAt") = date_series.month
+  GROUP BY
+    date_series.month
+  ORDER BY
+    date_series.month ASC
+`;
+  const mappedresult = result.map((datapoint) => {
+    return { date: datapoint.date, value: datapoint.value.toNumber() };
+  });
+  return mappedresult as TimeseriesDatapoint[];
+};
+
+/**
+ * Gets the total sum of deposits between the given dates grouped by day
+ * @param startDate
+ * @param endDate
+ * @returns {Promise<TimeseriesDatapoint[]>} The total sum of deposits between the given dates grouped by day
+ */
+export const getDepositStatsByDay = async (
+  startDate: Date,
+  endDate: Date,
+): Promise<TimeseriesDatapoint[]> => {
+  const result = await db.$queryRaw`
+    SELECT
+      date_series.date AS date,
+      COALESCE(SUM("amount"), 0) AS value
+    FROM
+      generate_series(${startDate}::date, ${endDate}::date, INTERVAL '1 day') AS date_series(date)
+    LEFT JOIN
+      "Deposit" ON DATE("createdAt") = date_series.date
+    GROUP BY
+      date_series.date
+    ORDER BY
+      date_series.date ASC
+  `;
+  const mappedresult = result.map((datapoint) => {
+    return { date: datapoint.date, value: datapoint.value.toNumber() };
+  });
+  return mappedresult as TimeseriesDatapoint[];
+};
+
+/**
+ * Gets the total sum of deposits between the given dates grouped by week
+ * @param startDate
+ * @param endDate
+ * @returns {Promise<TimeseriesDatapoint[]>} The total sum of deposits between the given dates grouped by week
+ */
+export const getDepositStatsByWeek = async (
+  startDate: Date,
+  endDate: Date,
+): Promise<TimeseriesDatapoint[]> => {
+  const result = await db.$queryRaw`
+    SELECT
+      week_series.week_start AS date,
+      COALESCE(SUM("amount"), 0) AS value
+    FROM
+      generate_series(
+        DATE_TRUNC('week', ${startDate}::date),
+        DATE_TRUNC('week', ${endDate}::date),
+        INTERVAL '1 week'
+      ) AS week_series(week_start)
+    LEFT JOIN
+      "Deposit" ON DATE_TRUNC('week', "Deposit"."createdAt") = week_series.week_start
+    GROUP BY
+      week_series.week_start
+    ORDER BY
+      week_series.week_start ASC
+  `;
+  const mappedresult = result.map((datapoint) => {
+    return { date: datapoint.date, value: datapoint.value.toNumber() };
+  });
+  return mappedresult as TimeseriesDatapoint[];
+};
+
+/**
+ * Gets the total sum of deposits between the given dates grouped by hour
+ * @param startDate
+ * @param endDate
+ * @returns {Promise<TimeseriesDatapoint[]>} The total sum of deposits between the given dates grouped by hour
+ */
+export const getDepositStatsByHour = async (
+  startDate: Date,
+  endDate: Date,
+): Promise<TimeseriesDatapoint[]> => {
+  const result = await db.$queryRaw`
+    SELECT
+      hour_series.hour_start AS date,
+      COALESCE(SUM("amount"), 0) AS value
+    FROM
+      generate_series(
+        ${startDate}::timestamp,
+        ${endDate}::timestamp,
+        INTERVAL '1 hour'
+      ) AS hour_series(hour_start)
+    LEFT JOIN
+      "Deposit" ON DATE_TRUNC('hour', "Deposit"."createdAt") = hour_series.hour_start
+    GROUP BY
+      hour_series.hour_start
+    ORDER BY
+      hour_series.hour_start ASC
+  `;
+  const mappedresult = result.map((datapoint) => {
+    return { date: datapoint.date, value: datapoint.value.toNumber() };
+  });
+  return mappedresult as TimeseriesDatapoint[];
 };
