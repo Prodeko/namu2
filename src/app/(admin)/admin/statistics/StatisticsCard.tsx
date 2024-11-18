07d88dc3866807d3a71ccc8a5033f8d6901468a5
ccruzkauppila <ccruzkauppila@live.com>
2024-11-18 18:02:03 +0000

    Make StatisticsCard title smaller

--- src/app/(admin)/admin/statistics/StatisticsCard.tsx
+++ src/app/(admin)/admin/statistics/StatisticsCard.tsx
@@ -3,7 +3,7 @@ import { ComponentPropsWithoutRef, ReactNode } from "react";
 import { cn } from "@/lib/utils";
 
 const containerStyles =
-  "w-full relative rounded-md divide-neutral-600 border-2 border-neutral-600 bg-white text-neutral-800 pt-10 mt-8";
+  "w-full relative rounded-md divide-neutral-600 border-2 border-neutral-600 bg-white text-neutral-800 pt-8 mt-8";
 
 interface Props extends ComponentPropsWithoutRef<"div"> {
   title: string;
@@ -13,7 +13,7 @@ interface Props extends ComponentPropsWithoutRef<"div"> {
 export const StatisticsCard = ({ title, children, ...props }: Props) => {
   return (
     <div className={cn(props.className, containerStyles)}>
-      <div className="absolute left-6 top-0 z-10 -translate-y-1/2 rounded-md border-2 border-neutral-600 bg-white px-6 py-5 text-3xl font-bold text-neutral-800  drop-shadow-[3px_3px_0px_theme(colors.neutral.700)]">
+      <div className="absolute left-6 top-0 z-10 -translate-y-1/2 rounded-md border-2 border-neutral-600 bg-white px-6 py-4 text-2xl font-bold text-neutral-800  drop-shadow-[3px_3px_0px_theme(colors.neutral.700)]">
         {title}
       </div>
       {children}
