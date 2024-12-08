0f6b84354f0839bf002f6ab7223e43726994fd6d
ccruzkauppila <ccruzkauppila@live.com>
2024-12-08 12:35:15 +0000

    Add KeyNumbersStackedBar component

---
+++ src/app/(admin)/admin/statistics/KeyNumbersStackedBar.tsx
@@ -0,0 +1,25 @@
+import { ComponentPropsWithoutRef } from "react";
+
+import { cn } from "@/lib/utils";
+
+import { SingleStackedBar } from "./charts/SingleStackedBar";
+
+interface Props extends ComponentPropsWithoutRef<"div"> {
+  title: string;
+  data: number[];
+  labels: string[];
+}
+
+export const KeyNumbersStackedBar = ({
+  title,
+  data,
+  labels,
+  ...props
+}: Props) => {
+  return (
+    <div className={cn("flex flex-col gap-1 p-4", props.className)}>
+      <p className="pb-2 font-bold">{title}</p>
+      <SingleStackedBar data={data} labels={labels} />
+    </div>
+  );
+};
