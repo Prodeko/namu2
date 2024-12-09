24cc7e721a7486d9c0d56f4059103c585021ea66
ccruzkauppila <ccruzkauppila@live.com>
2024-12-09 17:27:21 +0000

    Responsive design for depositNumbersCard

--- src/app/(admin)/admin/statistics/DepositNumbersCard.tsx
+++ src/app/(admin)/admin/statistics/DepositNumbersCard.tsx
@@ -9,9 +9,7 @@ import {
   getDepositStatsByMonth,
   getDepositStatsByWeek,
 } from "@/server/actions/stats/deposits";
-import {
-  TimeseriesDatapoint,
-} from "@/server/actions/stats/transactions";
+import { TimeseriesDatapoint } from "@/server/actions/stats/transactions";
 
 import { HeadlinerStatistic } from "./HeadlinerStatistic";
 import { ChartDataGetter } from "./SalesNumbersCard";
@@ -56,14 +54,14 @@ export const DepositNumbersCard = async ({ timeframe, ...props }: Props) => {
   const datapoints = chartData.map((p) => p.value);
   return (
     <StatisticsCard title="Deposits" className="grid w-full grid-cols-3">
-      <div className="col-span-2 flex flex-col p-4">
+      <div className="col-span-full flex flex-col p-2 lg:col-span-2 lg:p-4">
         <AdminBarChart
           data={datapoints}
           labels={chartLabels}
-          className="h-64 w-full self-center pl-6 pt-6"
+          className="w-full max-w-md self-center pl-0 pt-3 lg:h-64 lg:pl-6 lg:pt-6"
         />
       </div>
-      <div className="flex flex-col gap-6 px-4 py-10">
+      <div className="col-span-full flex flex-col gap-3 px-4 py-4 lg:col-span-1 lg:gap-6 lg:py-10">
         <HeadlinerStatistic
           title="Sum of deposits"
           value={formatCurrency(depositStats.sum)}
