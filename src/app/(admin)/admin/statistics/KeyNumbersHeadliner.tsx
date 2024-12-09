610fca6d7336d725e16a1d19438075e8e0a0dc83
ccruzkauppila <ccruzkauppila@live.com>
2024-12-09 17:42:57 +0000

    Responsive design for key figures

--- src/app/(admin)/admin/statistics/KeyNumbersHeadliner.tsx
+++ src/app/(admin)/admin/statistics/KeyNumbersHeadliner.tsx
@@ -5,9 +5,9 @@ interface Props {
 
 export const KeyNumbersHeadliner = ({ title, value }: Props) => {
   return (
-    <div className="flex flex-col gap-1 p-4">
-      <p className="col-span-2 font-bold">{title}</p>
-      <p className="text-2xl font-bold text-primary-400">{value}</p>
+    <div className="flex flex-col gap-1 px-4 py-3 lg:py-4">
+      <p className="col-span-2 text-sm font-bold lg:text-base">{title}</p>
+      <p className="text-xl font-bold text-primary-400 lg:text-2xl">{value}</p>
     </div>
   );
 };
