b099e39725fdced5739a39e7e89e9e47a92a3a9b
ccruzkauppila <ccruzkauppila@live.com>
2025-01-06 14:22:13 +0000

    Attempt to fix user purchase history page redirecting to login

--- src/app/(loggedin)/account/purchases/page.tsx
+++ src/app/(loggedin)/account/purchases/page.tsx
@@ -1,20 +1,18 @@
 import { redirect } from "next/navigation";
 
-import { getSession } from "@/auth/ironsession";
 import { formatCleverDateTime } from "@/common/utils";
 import { EmptyPage } from "@/components/ui/EmptyPage";
 import { HistoryList, HistoryListItem } from "@/components/ui/HistoryList";
 import { AccountHistoryLayout } from "@/components/ui/Layouts/AccountHistoryLayout";
+import { getCurrentUser } from "@/server/db/queries/account";
 import { getUserTransactionsWithItems } from "@/server/db/queries/transaction";
 
 const PurchaseHistoryPage = async () => {
-  const session = await getSession();
-  if (!session) {
+  const user = await getCurrentUser();
+  if (!user.ok) {
     redirect("/login");
   }
-  const purchaseHistory = await getUserTransactionsWithItems(
-    session.user.userId,
-  );
+  const purchaseHistory = await getUserTransactionsWithItems(user.user.id);
 
   return (
     <AccountHistoryLayout title="Purchase History">
