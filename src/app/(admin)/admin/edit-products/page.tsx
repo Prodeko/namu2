09e8797027243a5100fded76ed6dbd3c3c706e9f
ccruzkauppila <ccruzkauppila@live.com>
2025-01-12 17:47:59 +0000

    Remove redundant back button in admin page

--- src/app/(admin)/admin/edit-products/page.tsx
+++ src/app/(admin)/admin/edit-products/page.tsx
@@ -7,7 +7,7 @@ const Restock = async () => {
   const products = await getActiveClientProducts();
   return (
     <div className="no-scrollbar flex h-fit w-full max-w-screen-lg flex-col gap-4 overflow-y-scroll px-0 md:gap-8 lg:w-[80%]">
-      <AdminTitle withBackButton title="Products" />
+      <AdminTitle title="Products" />
       <AdminProductSection products={products} />
     </div>
   );
