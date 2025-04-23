3f29adf5acef85a77a3b2d34a24b4353c1cb2f9e
ccruzkauppila <ccruzkauppila@live.com>
2025-04-23 16:16:33 +0300

    Limit the purchase slider width to half the screen in landscape mode, fixes #79

--- src/app/(loggedin)/shop/page.tsx
+++ src/app/(loggedin)/shop/page.tsx
@@ -26,7 +26,7 @@ const Shop = async () => {
         <ProductSection section={sections.drinks} items={drinks} />
         <ProductSection section={sections.snacks} items={snacks} />
         <ProductSection section={sections.food} items={food} />
-        <div className="fixed left-0 top-[100dvh] flex w-full max-w-[100vw] -translate-y-full flex-col-reverse gap-2 bg-[linear-gradient(to_top,theme(colors.neutral.700/50%),theme(colors.neutral.50/0%))] px-5 pb-5 md:flex-row md:gap-4 md:p-12">
+        <div className="fixed left-0 top-[100dvh] flex w-full max-w-[100vw] -translate-y-full flex-col-reverse justify-center gap-2 bg-[linear-gradient(to_top,theme(colors.neutral.700/50%),theme(colors.neutral.50/0%))] px-5 pb-5 md:flex-row md:gap-4 md:p-12">
           <PurchaseSlider />
           <ShoppingCart />
         </div>
